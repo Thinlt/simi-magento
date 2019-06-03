da9153688cd1838d556849810fad7fd973e84e2b
Cody Nguyen <cody@simicart.com>
2019-06-03 15:07:50 +0700

    First commit

---
+++ dist/precache-manifest.57f2d1d6f869bfdf55380daf3cb437a1.js
@@ -0,0 +1,9 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fde7d24060a7dec8c2a4",
+    "url": "/js/client.js"
+  },
+  {
+    "url": "/js/vendor~client-ec32a807c146ef258407.js"
+  }
+]);
---
+++ esm/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { setContext } from 'apollo-link-context';
+import { Util, WindowSizeContextProvider } from '@magento/peregrine';
+const {
+  BrowserPersistence
+} = Util;
+const apiBase = new URL('/graphql', location.origin).toString();
+/**
+ * The Siminia adapter provides basic context objects: a router, a store, a
+ * GraphQL client, and some common functions. It is not opinionated about auth,
+ * so we add an auth implementation here and prepend it to the Apollo Link list.
+ */
+
+ReactDOM.render('Hello world', document.getElementById('root'));
+
+if (process.env.SERVICE_WORKER && 'serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register(process.env.SERVICE_WORKER).then(registration => {
+      console.log('Service worker registered: ', registration);
+    }).catch(error => {
+      console.log('Service worker registration failed: ', error);
+    });
+  });
+}
+
+window.addEventListener('online', () => {
+  store.dispatch(app.setOnline());
+});
+window.addEventListener('offline', () => {
+  store.dispatch(app.setOffline());
+});
+//# sourceMappingURL=index.js.map
