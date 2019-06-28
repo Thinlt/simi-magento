e4d4d7f60552fcd7005f9726a469abb05d264999
Cody Nguyen <cody@simicart.com>
2019-06-28 20:00:45 +0700

    Category header

--- src/simi/App/core/RootComponents/Category/categoryHeader.js
+++ src/simi/App/core/RootComponents/Category/categoryHeader.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
 const CategoryHeader = props => {
-    const { classes, name, image_url } = props
+    const { name, image_url } = props
     return (
-        <div className={classes['category-header']}>
+        <div style={{width: '100%', overflow: 'hidden'}}>
             <img
                 alt={name}
                 src={image_url}
+                style={{width: '100%'}}
             />
         </div>
     )
