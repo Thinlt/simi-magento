08b5cc13b1fe949cea9fca7b31f8e89bad1bd8a8
hung <maiquochung26@gmail.com>
2019-07-09 12:04:30 +0700

    customer dashboard

---
+++ src/simi/App/core/Customer/Account/Components/Orders/PaginationTable.js
@@ -0,0 +1,160 @@
+import React, { Component } from 'react'
+import Pagination from 'src/simi/BaseComponents/Pagination'
+import Identify from 'src/simi/Helper/Identify'
+import Arrow from "src/simi/BaseComponents/Icon/Arrow";
+
+class PaginationTable extends Pagination {
+    constructor(props) {
+        super(props)
+        this.startPage = 1;
+        this.endPage = this.startPage + 2;
+    }
+
+    renderColumnTitle = () => {
+        let data = this.props.cols;
+        if(data.length > 0){
+            let columns = data.map((item, index)=>{
+                return <th key={index} width={item.width?item.width: ''} >{Identify.__(item.title)}</th>
+            });
+            return (
+                <thead>
+                    <tr>
+                        {columns}
+                    </tr>
+                </thead>
+            ) 
+            
+        }
+    }
+
+    changePage(event) {
+        this.changePage({
+            currentPage: Number(event.target.id)
+        })
+    }
+
+    handleChangePage =(next = true, total)=>{
+        let currentPage = next ? (this.state.currentPage === total?this.state.currentPage: this.state.currentPage + 1) : (this.state.currentPage> 1 ? this.state.currentPage - 1: this.state.currentPage);
+        if(currentPage > this.endPage){
+            this.startPage = this.startPage + 1;
+            this.endPage = this.endPage + 1;
+        }else if (currentPage < this.startPage){
+            this.startPage = this.startPage - 1;
+            this.endPage = this.endPage - 1;
+        }
+        this.setState({
+            currentPage : currentPage
+        })
+    }
+
+    renderPageNumber = (total)=> {
+        // Logic for displaying page numbers
+        const { classes } = this.props;
+        if(!this.props.showPageNumber) return null;
+        const pageNumbers = [];
+        let totalItem = total;
+        total =  Math.ceil(total / this.state.limit);
+        let endpage = this.endPage > total ? total : this.endPage
+        for (let i = this.startPage; i <= endpage; i++) {
+            pageNumbers.push(i);
+        }
+        let obj = this;
+        const renderPageNumbers = pageNumbers.map(number => {
+            let active = number === obj.state.currentPage ? 'active': '';
+            return (
+                <li
+                    key={number}
+                    id={number}
+                    onClick={(e)=>this.changePage(e)}
+                    className={active}
+                >
+                    {number}
+                </li>
+            );
+        });
+        let option_limit = [];
+        if (this.props.itemsPerPageOptions)
+        {
+            this.props.itemsPerPageOptions.map((item, index) => {
+                    option_limit.push(<option key={index} value={item} >{item}</option>);
+                    return null 
+                }
+            );
+        }
+        let nextPageIcon = <Arrow style={{width: 20, height: 20, transform: 'rotate(90deg)'}}/>;
+        let prevPageIcon = <Arrow style={{width: 20, height: 20, transform: 'rotate(-90deg)'}}/>;
+
+        let pagesSelection = (total>1)?(
+            <ul id="page-numbers" style={{
+                border : 'none',
+                padding : 0,
+                display : 'flex',
+                alignItems : 'center',
+                fontSize : 14
+            }}>
+                <li className={classes["icon-page-number"]} onClick={()=>this.handleChangePage(false, total)}>{prevPageIcon}</li>
+                {renderPageNumbers}
+                <li className={classes["icon-page-number"]} onClick={()=>this.handleChangePage(true, total)}>{nextPageIcon}</li>
+            </ul>
+        ):'';
+        let {currentPage,limit} = this.state;
+        let lastItem = currentPage * limit;
+        let firstItem = lastItem - limit+1;
+        lastItem = lastItem > totalItem ? totalItem : lastItem;
+        let itemsPerPage = (
+            <div className={classes["icon-page-number"]}>
+                {
+                    this.props.showInfoItem &&
+                    <span style={{marginRight : 10,fontSize : 16}}>
+                        {Identify.__('%a - %b of %c').replace('%a', firstItem).replace('%b', lastItem).replace('%c', totalItem)}
+                    </span>
+                }
+            </div>
+        );
+        return (
+            <div className={classes["config-page"]}
+                 style={{
+                     display : 'flex',
+                     alignItems : 'center',
+                     justifyContent : 'space-between',
+                     clear: 'both'
+                 }}
+            >
+                {itemsPerPage}
+                {pagesSelection}
+            </div>
+        )
+    }
+
+    renderPagination = () => {
+        let {data, currentPage, limit} = this.state;
+        const {classes} = this.props;
+        if(data.length > 0){
+            // Logic for displaying current todos
+            const indexOfLastTodo = currentPage * limit;
+            const indexOfFirstTodo = indexOfLastTodo - limit;
+            const currentReview = data.slice(indexOfFirstTodo, indexOfLastTodo);
+            let obj = this;
+            const items = currentReview.map((item, key) => {
+                return obj.renderItem(item, key);
+            });
+            let total = data.length;
+            return (
+                <React.Fragment>
+                    <table className={`col-xs-12 ${classes["table-striped"]} ${classes["table-siminia"]}`}>
+                        {this.renderColumnTitle()}
+                        <tbody>{items}</tbody>
+                    </table>
+                    {this.renderPageNumber(total)}
+                </React.Fragment>
+            )
+        }
+        return <div></div>
+    }
+
+    render() {
+        return this.renderPagination();
+    }
+}
+
+export default PaginationTable
--- src/simi/App/core/Customer/Account/Page/Dashboard/index.js
+++ src/simi/App/core/Customer/Account/Page/Dashboard/index.js
@@ -2,12 +2,67 @@ import React, { useEffect, useState } from 'react';
 import Identify from "src/simi/Helper/Identify";
 import {Link} from 'react-router-dom'
 import { Whitebtn } from 'src/simi/BaseComponents/Button';
-import { Query } from 'src/drivers';
-import { SimiMutation } from 'src/simi/Network/Query'
+import OrderHistory from 'src/simi/App/core/Customer/Account/Components/Orders/OrderList';
 import Loading from "src/simi/BaseComponents/Loading";
+import { simiUseQuery } from 'src/simi/Network/Query' 
+import getCustomerInfoQuery from 'src/simi/queries/getCustomerInfo.graphql'
+import AddressItem from './AddressItem'; 
 
 const Dashboard = props => {
     const {classes, history, isPhone, customer} = props;
+    const [queryResult, queryApi] = simiUseQuery(getCustomerInfoQuery, false);
+    const {data} = queryResult
+    const { runQuery } = queryApi;
+
+    const getCustomerInfo = () => {
+        runQuery({});
+    }
+
+    useEffect(() => {
+        if(!data) {
+            getCustomerInfo();
+        }
+    }, [data])
+
+    const renderDefaultAddress = (item, default_billing, default_shipping) => {
+        let defaultBilling = item.find(value => {
+            return value.id = default_billing
+        });
+
+        const defaultShipping = item.find(value => {
+            return value.id = default_shipping
+        })
+        
+        return (
+            <div className={classes["address-book__container"]}>
+                <div className={classes["dash-column-box"]}>
+                    <div className={classes["box-title"]}>
+                        {Identify.__("Default Billing Address")}
+                    </div>
+                    <AddressItem
+                        addressData={defaultBilling}
+                        classes={classes}
+                    />
+                    <Link className={classes["edit-item"]} to={`/addresses.html/${defaultBilling.id}`}>{Identify.__("Edit address")}</Link>
+                </div>
+                <div className={classes["dash-column-box"]}>
+                    <div className={classes["box-title"]}>
+                        {Identify.__("Default Shipping Address")}
+                    </div>
+                    <AddressItem
+                        addressData={defaultShipping}
+                        classes={classes}
+                    />
+                    <Link className={classes["edit-item"]} to={`/addresses.html/${defaultShipping.id}`}>{Identify.__("Edit address")}</Link>
+                </div>
+            </div>
+        );
+    }
+
+    if(!data) {
+        return <Loading />
+    }
+
     return (
         <div className={classes['my-dashboard']}>
             {!isPhone ? (
@@ -16,7 +71,7 @@ const Dashboard = props => {
                             {Identify.__("Recent Orders")}
                             <Link className={classes["view-all"]} to='/orderhistory.html'>{Identify.__("View all")}</Link>
                         </div>
-                        {/* <OrderHistory parent={this} showForDashboard={true} /> */}
+                        <OrderHistory classes={classes} data={data} showForDashboard={true} />
                     </div>
                 ) : (
                     <Link to="/orderhistory.html">
@@ -37,7 +92,7 @@ const Dashboard = props => {
                             <div className={classes["box-title"]}>
                                 {Identify.__("Contact information")}
                             </div>
-                            <p className={classes["desc"]}>{customer.email}</p>
+                            <p className={`${classes["desc"]} ${classes["email"]}`}>{customer.email}</p>
                             <Link className={classes["edit-link"]} to={{ pathname: '/profile.html', state: {profile_edit: 'password'} }}>{Identify.__("Change password")}</Link>
                         </div>
                         <Link to="/profile.html">
@@ -74,6 +129,15 @@ const Dashboard = props => {
                     </div>
                 </div>
             </div>
+            {data.customer && data.customer.addresses && (
+                <div className={classes["dashboard-address-book"]}>
+                    <div className={classes["customer-page-title"]}>
+                        {Identify.__("Address Book")}
+                        <Link className={classes["view-all"]} to="/addresses.html">{Identify.__("Manage addresses")}</Link>
+                    </div>
+                    {renderDefaultAddress(data.customer.addresses, data.customer.default_billing, data.customer.default_shipping)}
+                </div>
+            )} 
         </div>
     )
     
