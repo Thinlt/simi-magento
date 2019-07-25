b0af169c9ccfa5eb01376390c9955214ab6f344c
Cody Nguyen <cody@simicart.com>
2019-07-25 15:02:54 +0700

    Change navigation workflow

--- src/simi/App/core/Navigation/index.js
+++ src/simi/App/core/Navigation/index.js
@@ -1,2 +1,24 @@
-export { default } from './container';
 export { default as Navigation } from './navigation';
+
+import { connect } from 'src/drivers';
+import { closeDrawer } from 'src/actions/app';
+import { getUserDetails } from 'src/actions/user';
+import Navigation from './navigation';
+
+const mapStateToProps = ({ app, user }) => {
+    const { currentUser } = user;
+    const { drawer } = app
+    return {
+        drawer,
+        currentUser
+    }
+}
+const mapDispatchToProps = {
+    closeDrawer,
+    getUserDetails
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Navigation);
