e637734e2ad6d9d6eba2f46209ef4e0a73310254
Robert <robert@Roberts-MacBook-Pro.local>
2019-09-24 10:18:17 +0700

    Add vendor_name to simiExtraField product search and detail

---
+++ app/code/Simi/VendorMapping/Observer/SimiSimiconnectorGraphqlSimiProductListItemAfter.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Simi\VendorMapping\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+
+class SimiSimiconnectorGraphqlSimiProductListItemAfter implements ObserverInterface {
+
+    /**
+     * Add vendor_name to SimiProductListItemExtraField
+     */
+    public function execute(\Magento\Framework\Event\Observer $observer) {
+        $object = $observer->getObject();
+        // $extraData = $observer->getExtraData();
+        if (isset($object->productExtraData['attribute_values']['vendor_id'])) {
+            if (class_exists('Vnecoms\Vendors\Model\Vendor')) {
+
+                // var_dump($object->productExtraData['attribute_values']['vendor_id']);//die;
+
+                $vendor = \Magento\Framework\App\ObjectManager::getInstance()
+                    ->get(\Vnecoms\Vendors\Model\Vendor::class)
+                    ->load($object->productExtraData['attribute_values']['vendor_id']);
+                if ($vendor->getId()) {
+                    $vendor_name = $vendor->getName();
+                    // productExtraData
+                    $object->productExtraData['attribute_values']['vendor_name'] = $vendor_name;
+                }
+            }
+        }
+        return $this;
+    }
+}
---
+++ app/code/Simi/VendorMapping/Observer/SimiconnectorGetStoreviewInfoAfter.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Simi\VendorMapping\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+
+class SimiconnectorGetStoreviewInfoAfter implements ObserverInterface {
+
+    // public function execute(\Magento\Framework\Event\Observer $observer) {
+    //     // add giftcard product timezone options
+    //     $object = $observer->getObject();
+    //     // storeviewInfo
+    //     $object->storeviewInfo['vendor_list'] = ['vendor 1', 'vendor 2'];
+    //     return $this;
+    // }
+}
