ab4b37c4b063e3c7412d197b8cd77061f2b2574e
huykon225 <huykon225@gmail.com>
2019-09-27 10:18:51 +0700

    update

--- src/simi/App/core/Checkout/OrderSummary/OrderSummary.js
+++ src/simi/App/core/Checkout/OrderSummary/OrderSummary.js
@@ -1,17 +1,13 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 import Panel from 'src/simi/BaseComponents/Panel';
 import Identify from 'src/simi/Helper/Identify';
-import { configColor } from 'src/simi/Config';
-import { Price } from '@magento/peregrine';
 import Arrow from 'src/simi/BaseComponents/Icon/Arrowup';
 import Total from 'src/simi/BaseComponents/Total';
 import isObjectEmpty from 'src/util/isObjectEmpty';
 import AddressItem from 'src/simi/BaseComponents/Address';
-import { resourceUrl, logoUrl } from 'src/simi/Helper/Url';
-import Image from 'src/simi/BaseComponents/Image';
+import OrderItems from './OrderItems';
 
 require('./OrderSummary.scss')
-
 const $ = window.$;
 
 const OrderSummary = (props) => {
@@ -24,70 +20,12 @@ const OrderSummary = (props) => {
 
     const { is_virtual } = details;
 
-    const orderItem = details && details.items && details.items.map(o_item => {
-        let itemsOption = '';
-        let optionElement = ''
-        if (o_item.options.length > 0) {
-            itemsOption = o_item.options.map((optionObject) => {
-                return (
-                    <div key={Identify.randomString()}>
-                        <span className='option-title'>{optionObject.label}: </span>
-                        <span className='option-value'>{optionObject.value}</span>
-                    </div>
-                );
-            });
-
-            optionElement = (
-                <div className='item-options'>
-                    <div className='show-label' onClick={(e) => handleToggleOption(e)} role="presentation">
-                        <span>{Identify.__('See details')}</span>
-                        <Arrow className='arrow-down' />
-                    </div>
-                    <div className={'options-selected'} style={{ display: 'none' }}>
-                        {itemsOption}
-                    </div>
-                </div>
-            );
-        }
-        const image = (o_item.image && o_item.image.file) ? o_item.image.file : o_item.simi_image
-
-        return (
-            <li key={Identify.randomString()} className='order-item'>
-                <div className='item-image' style={{ borderColor: configColor.image_border_color }}>
-                    <Image
-                        src={
-                            image ?
-                                resourceUrl(image, {
-                                    type: 'image-product',
-                                    width: 80
-                                }) :
-                                logoUrl()
-                        }
-                        alt={o_item.name} />
-                </div>
-                <div className='item-info' style={{ width: '100%' }}>
-                    <label className='item-name'>{o_item.name}</label>
-                    <div className='item-qty-price'>
-                        <span className='qty'>{Identify.__("Qty")}: {o_item.qty}</span>
-                        <span className='price'><Price currencyCode={cartCurrencyCode} value={o_item.price} /></span>
-                    </div>
-                    {optionElement}
-                </div>
-            </li>
-        );
-
-    })
+    const orderItem = useMemo(() => details && details.items && <OrderItems items={details.items} cartCurrencyCode={cartCurrencyCode} />, [details.items]);
 
     const handleToggleItems = (e) => {
         const parent = $(e.currentTarget);
         parent.next('ul').slideToggle('fast');
         parent.find('.expand_icon').toggleClass('rotate-180')
-    }
-
-    const handleToggleOption = (e) => {
-        const parent = $(e.currentTarget);
-        parent.next('.options-selected').slideToggle('fast');
-        parent.find('svg').toggleClass('rotate-0');
     }
 
     const totalsSummary = (
