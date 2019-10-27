0acb352526ef39104f41eb72b5b85e300f180f31
Robert <robert@Roberts-MacBook-Pro.local>
2019-10-27 16:27:42 +0700

    Fix shipping method change input type

--- src/simi/App/Bianca/BaseComponents/Checkbox/index.js
+++ src/simi/App/Bianca/BaseComponents/Checkbox/index.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from 'react';
+import Identify from 'src/simi/Helper/Identify';
 require ('./index.scss')
 
 export const Checkbox = (props) => {
+
+    const handleOnChange = (e) => {
+        if (props.onChange) {
+            props.onChange(e.target.value);
+        }
+    }
+
     return (
         <div 
             {...props}
@@ -13,6 +21,7 @@ export const Checkbox = (props) => {
             <span className="checkbox-item-text">
                 {props.label}
             </span>
+            <input onChange={handleOnChange} name={props.name || Identify.randomString(3)} style={{display: 'none'}} type="radio" value={props.value} checked={props.selected}/>
         </div>
     )
 }
--- src/simi/App/Bianca/Checkout/ShippingForm/ShippingForm.js
+++ src/simi/App/Bianca/Checkout/ShippingForm/ShippingForm.js
@@ -1,226 +1,220 @@
-import React, { useCallback, useEffect, useState, useMemo } from 'react';
+import React, {useEffect, useState, useMemo } from 'react';
 // import { Form } from 'informed';
 import { array, func, shape, string } from 'prop-types';
 import { formatLabelPrice } from 'src/simi/Helper/Pricing';
 import Identify from 'src/simi/Helper/Identify';
-import FieldShippingMethod from 'src/simi/App/Bianca/Checkout/components/fieldShippingMethod';
+// import FieldShippingMethod from 'src/simi/App/Bianca/Checkout/components/fieldShippingMethod';
 import Checkbox from 'src/simi/App/Bianca/BaseComponents/Checkbox'
 import Loading from 'src/simi/BaseComponents/Loading/ReactLoading';
 import { request } from 'src/simi/Network/RestMagento';
+import { Util } from '@magento/peregrine';
 require('./ShippingForm.scss')
 
 const SHIPPING_METHOD_SELECTED = 'shipping_method_selected';
 
+/**
+ * 
+ * @param {vendor} ids 
+ */
+const useLoadVendor = (availableVendorsMethods) => {
+    const [vendors, setVendors] = useState();
+    const getVendors = async function(vendorIds) {
+        const vendorApi = '/rest/V1/simiconnector/vendors';
+        const response = await request(vendorApi, {
+            method: 'POST',
+            body: JSON.stringify({
+                ids: vendorIds.join(',')
+            })
+        });
+        return response;
+    }
+    const loadVendorApi = async () => {
+        if (availableVendorsMethods && availableVendorsMethods.length && !vendors) {
+            let vendorIds = availableVendorsMethods.map((vendorMethods) => {
+                return vendorMethods.vendor_id;
+            });
+            let response = await getVendors(vendorIds);
+            if (response) {
+                setVendors(response);
+            }
+        }
+    };
+    useEffect(() => {
+        loadVendorApi();
+        return () => {
+            const controller = new AbortController();
+            controller.abort(); //cancel requesting
+        }
+    }, []);
+    return vendors;
+}
+
 const ShippingForm = (props) => {
+    const { BrowserPersistence } = Util;
+    const storage = new BrowserPersistence();
     const {
         availableShippingMethods,
         cancel,
         shippingMethod,
         submit
     } = props;
-    let [vendors, setVendors] = useState([]); // vendor list
-    const [selectedValue, setSelectedValue] = useState(null)
-    useEffect(() => {
-        async function getVendors(vendorIds) {
-            const vendorApi = '/rest/V1/simiconnector/vendors';
-            const response = await request(vendorApi, {
-                method: 'POST',
-                body: JSON.stringify({
-                    ids: vendorIds.join(',')
-                })
-            });
-            setVendors(response);
-            return response;
-        }
-        getVendors(vendorIds);
 
-        return () => {
-            const controller = new AbortController();
-            controller.abort(); //cancel requesting
+    // load method selected from storage
+    const storedShippingMethod = storage.getItem('shippingMethod');
+    // const initialValue = Identify.getDataFromStoreage(Identify.SESSION_STOREAGE, SHIPPING_METHOD_SELECTED);
+    let initialSelected = {}
+    if (storedShippingMethod && storedShippingMethod.method_code) {
+        let selectedMethodsArray = storedShippingMethod.method_code.replace(/vflatrate_/g, '').split('|_|');
+        for(let i = 0; i < selectedMethodsArray.length; i++){
+            let vendorIdRate = selectedMethodsArray[i].split('||');
+            if (vendorIdRate[1] != undefined) {
+                initialSelected[vendorIdRate[1]] = selectedMethodsArray[i];
+            }
         }
-    }, []);
+    }
+    const [methodCodesSelected, setMethodCodesSelected] = useState(initialSelected);
+    let availableVendorsMethods = [];
+    const defaultMethod = { value: '', label: Identify.__('Please choose') }
+    const vendors = useLoadVendor(availableVendorsMethods);
 
-    const handleSubmit = useCallback(
-        (shippingMethod) => {
-            let selectedShippingMethod = availableShippingMethods.find(
-                ({ method_code }) => method_code === shippingMethod
+    // convert availableShippingMethods to vendor shipping methods
+    availableVendorsMethods = useMemo(() => {
+        if (availableShippingMethods.length) {
+            availableShippingMethods.map(
+                (shippingMethod) => {
+                    // availableVendorsMethods
+                    let { carrier_code, carrier_title, method_code, method_title, price_incl_tax } = shippingMethod;
+                    if (carrier_code === "vendor_multirate") {
+                        return false
+                    }
+                    
+                    if (method_code) {
+                        let methodCode = method_code.split('||');
+                        if (methodCode[1] !== undefined){
+                            let index;
+                            let vendorMethod = availableVendorsMethods.find((vendor, i) => {
+                                if (vendor.vendor_id === methodCode[1]) {
+                                    index = i;
+                                    return true;
+                                }
+                                return false;
+                            });
+                            let rate = {
+                                id: methodCode[0],
+                                order: price_incl_tax,
+                                value: method_code,
+                                label: `${method_title} (${formatLabelPrice(price_incl_tax)})`
+                            }
+                            if (vendorMethod === undefined) {
+                                availableVendorsMethods.push({
+                                    vendor_id: methodCode[1],
+                                    carrier_code: carrier_code,
+                                    carrier_title: carrier_title,
+                                    rates: [rate]
+                                });
+                            }
+                            if (vendorMethod) {
+                                if (vendorMethod.rates) vendorMethod.rates.push(rate);
+                                availableVendorsMethods[index].rates = vendorMethod.rates;
+                            }
+                        }
+                    }
+                    return shippingMethod;
+                }
             );
-            if (!selectedShippingMethod) {
+            if (!availableVendorsMethods) return [];
+            return availableVendorsMethods;
+        }
+        return [];
+    }, [availableShippingMethods]);
+
+    const handleSubmit2 = (selectedMethods) => {
+        // get multi vendor shipping method
+        let vendor_multirate = ''; //multiple rates code
+        let selecteds = Object.values(selectedMethods); //array
+        if (availableVendorsMethods && availableVendorsMethods.length === selecteds.length) {
+            vendor_multirate = selecteds.join('|_|');
+            // console.log('SELECTED SHIPPING CODE: ', vendor_multirate);
+            // find method object available
+            let shippingMethod = availableShippingMethods.find(
+                ({ method_code, carrier_code }) => {
+                    if (carrier_code !== "vendor_multirate") {
+                        return false
+                    }
+                    if (selecteds.length === method_code.split('|_|').length) {
+                        for (let code in selectedMethods) {
+                            if (method_code.indexOf(selectedMethods[code]) === -1) {
+                                return false;
+                            }
+                        }
+                        return true;
+                    }
+                    return false;
+                }
+            );
+            // console.log('FOUND SHIPPING METHHOD: ', shippingMethod);
+            if (shippingMethod) {
+                Identify.storeDataToStoreage(Identify.SESSION_STOREAGE, SHIPPING_METHOD_SELECTED, shippingMethod.method_code);
+                submit({ shippingMethod: shippingMethod });
+            }
+            else {
                 console.warn(
-                    `Could not find the selected shipping method ${selectedShippingMethod} in the list of available shipping methods.`
+                    `Could not find the selected shipping method ${vendor_multirate} in the list of available shipping methods.`
                 );
                 cancel();
                 return;
             }
-            // selected method from Session storeage
-            if (initialValue) {
-                let selectedMethodsSessionStoreage = initialValue.replace(/vflatrate_/g, '').split('|_|');
-                for (let vendorId in selectableShippingMethodsVendors){
-                    let foundSelected = selectedMethodsSessionStoreage.find((method_code) => {
-                        let vendorIdSelected = method_code.split('||')[1] ? method_code.split('||')[1] : 0;
-                        if (vendorId === vendorIdSelected) return true;
-                        return false;
-                    });
-                    if (foundSelected) {
-                        selectableShippingMethodsVendors[vendorId].selectedMethod = foundSelected;
-                    }
-                }
-            }
-            //build selected selectedVendorsMethods
-            let rateVendorId = shippingMethod.split('||');
-            if (rateVendorId[1] != undefined) {
-                selectableShippingMethodsVendors[rateVendorId[1]].selectedMethod = shippingMethod;
-            } else {
-                selectedShippingMethod = shippingMethod;
-            }
-            let selectedMethodArray = Object.values(selectableShippingMethodsVendors);
-            let canSubmit = true;
-            selectedShippingMethod = selectedMethodArray.filter((methodItem) => {
-                if(methodItem.selectedMethod) return true;
-                canSubmit = false; //all vendor must selected shipping method
-                return false;
-            }).map((vendorMethod) => {
-                if (vendorMethod.selectedMethod) {
-                    return vendorMethod.selectedMethod;
-                }
-                return undefined;
-            })
-            // selectedShippingMethod = selectedShippingMethod.join('|_|');
-            if (canSubmit) {
-                // get multi vendor shipping method
-                let vendor_multirate_method = availableShippingMethods.find(
-                    ({ method_code }) => {
-                        if (selectedShippingMethod.length === method_code.split('|_|').length) {
-                            for(let i = 0; i < selectedShippingMethod.length; i++){
-                                if (method_code.indexOf(selectedShippingMethod[i]) === -1) {
-                                    return false;
-                                }
-                            }
-                            return true;
-                        }
-                        return false;
-                    }
-                );
-                if (vendor_multirate_method) {
-                    console.log(vendor_multirate_method);
-                    Identify.storeDataToStoreage(Identify.SESSION_STOREAGE, SHIPPING_METHOD_SELECTED, vendor_multirate_method.method_code);
-                    submit({ shippingMethod: vendor_multirate_method });
-                }
-            }
-        },
-        [availableShippingMethods, cancel, submit]
-    );
-
-
-    const initialValue = Identify.getDataFromStoreage(Identify.SESSION_STOREAGE, SHIPPING_METHOD_SELECTED);
-    let selectableShippingMethods;
-    let selectableShippingMethodsVendors = {};
-    let vendorIds = [];
-
-    const defaultMethod = { value: '', label: Identify.__('Please choose') }
-
-    if (availableShippingMethods.length) {
-        selectableShippingMethods = availableShippingMethods.map(
-            (methodRate) => {
-                let { carrier_code, carrier_title, method_code, method_title, price_excl_tax } = methodRate;
-                if (carrier_code === "vendor_multirate") {
-                    return false
-                }
-                if (method_code) {
-                    let rateVendorId = method_code.split('||');
-                    if (rateVendorId[1] !== undefined){
-                        if (vendorIds.indexOf(rateVendorId[1]) === -1) vendorIds.push(rateVendorId[1]);
-                        if (selectableShippingMethodsVendors[rateVendorId[1]] == undefined)
-                            selectableShippingMethodsVendors[rateVendorId[1]] = [];
-                        selectableShippingMethodsVendors[rateVendorId[1]].push({
-                            id: rateVendorId[0],
-                            order: price_excl_tax,
-                            value: method_code,
-                            label: `${method_title} (${formatLabelPrice(price_excl_tax)})`
-                        });
-                    }
-                }
-                return {value: carrier_code, label: `${carrier_title} (${formatLabelPrice(price_excl_tax)})`}
-            }
-        );
-        // initialValue = shippingMethod
-    } else {
-        selectableShippingMethods = [];
-        // initialValue = '';
-        return <Loading />
-    }
-
-
-    let selectableShippingMethodsVendorsArray = Object.values(selectableShippingMethodsVendors);
-
-    selectableShippingMethods.unshift(defaultMethod);
-
-    const childFieldProps = {
-        // initialValue,
-        // selectableShippingMethods,
-        availableShippingMethods,
-        submit,
-        cancel
-    }
-
-    // convert initialValue to methods
-    let selectedMethods = {}
-    if (initialValue) {
-        let selectedMethodsArray = initialValue.replace(/vflatrate_/g, '').split('|_|');
-        for(let i = 0; i < selectedMethodsArray.length; i++){
-            let vendorIdRate = selectedMethodsArray[i].split('||');
-            if (vendorIdRate[1] != undefined) {
-                selectedMethods[vendorIdRate[1]] = selectedMethodsArray[i];
-            }
+            return true;
         }
+        return false;
+    }
+
+    /**
+     * Handle select shipping method
+     * @param {*} action 
+     */
+    const methodSelecteHandle = (action) => {
+        let selecteds = {...methodCodesSelected};
+        selecteds[action.vendor_id] = action.method_code;
+        setMethodCodesSelected(selecteds);
+        handleSubmit2(selecteds);
+    }
+
+    if (!availableVendorsMethods || !availableVendorsMethods.length) {
+        return <Loading />
     }
 
     return (
         <form className="shipping-form">
             <div className="shipping-body">
                 {
-                    selectableShippingMethodsVendorsArray.map((methods, key) => {
-                        methods.sort((a, b) => (a.order > b.order) ? 1 : -1); //sort
-                        methods.unshift(defaultMethod);
-                        // let selectedValue = '';
-                        let vendorId;
-                        // if (methods[1]) {
-                        //     vendorId = methods[1].value.split('||')[1] ? methods[1].value.split('||')[1] : 0;
-                        //     selectedValue = selectedMethods[vendorId] ? selectedMethods[vendorId] : ''
-                        // }
-                        let vendorName = ''
+                    availableVendorsMethods.map((vendor, vendor_key) => {
+                        let {rates, carrier_title, vendor_id} = vendor;
+                        rates.sort((a, b) => (a.order > b.order) ? 1 : -1); //sort
+                        rates.unshift(defaultMethod);
+                        let vendorName = carrier_title && vendor_id !== 'default' && Identify.__(`Vendor ${vendor_id}`) || Identify.__('Default');
                         if(vendors){
-                            const vendor = vendors.find(({entity_id}) => parseInt(entity_id) === parseInt(vendorId));
+                            const vendor = vendors.find(({entity_id}) => parseInt(entity_id) === parseInt(vendor_id));
                             vendorName = vendor ? (vendor.firstname + (vendor.lastname ? ` ${vendor.lastname}` : '')) : Identify.__('Default');
                         }
-                        const vendor = vendors && vendors.find(({entity_id}) => parseInt(entity_id) === parseInt(vendorId));
-                        vendorName = vendor ? (vendor.firstname + (vendor.lastname ? ` ${vendor.lastname}` : '')) : Identify.__('Default');
-
-                        return <div key={key}>
+                        return <div key={vendor_key}>
                             <span>{vendorName}</span>
-                            {/* <FieldShippingMethod 
-                                handleSelect={(selectedMethod) => handleSubmit(selectedMethod)} 
-                                initialValue={selectedValue}
-                                selectableShippingMethods={methods} 
-                                {...childFieldProps} /> */}
-                            {methods.map((method)=>{
-                                if(!method.id){
+                            {rates.map((rate) => {
+                                if(!rate.id){
                                     return null;
                                 }
+                                let selected = (methodCodesSelected[vendor_id] === rate.value);
                                 return(
                                     <Checkbox 
-                                        key={method.id} 
-                                        label={method.label} 
-                                        value={method.value} 
-                                        onClick={() => setSelectedValue(method.value)}
-                                        onChange={() => {
-                                            handleSubmit(selectedValue)
-                                        }} 
-                                        selected={selectedValue === method.value ? true : false}
+                                        key={rate.id} 
+                                        label={rate.label} 
+                                        value={rate.value} 
+                                        onClick={() => methodSelecteHandle({vendor_id, method_code: rate.value})}
+                                        // onChange={(value) => handleSubmit(value)}
+                                        selected={selected}
                                     />
                                 )
-                            })
-                            }
+                            })}
                         </div>
                     })
                 }
