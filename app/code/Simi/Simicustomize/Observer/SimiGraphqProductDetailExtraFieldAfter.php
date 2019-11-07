588b6ae0dcdec6fca145dadcd174e579ab7143f7
Robert <robert@Roberts-MacBook-Pro.local>
2019-11-07 11:54:34 +0700

    Add color swatch image

--- app/code/Simi/Simicustomize/Observer/SimiGraphqProductDetailExtraFieldAfter.php
+++ app/code/Simi/Simicustomize/Observer/SimiGraphqProductDetailExtraFieldAfter.php
@@ -15,6 +15,22 @@ class SimiGraphqProductDetailExtraFieldAfter implements ObserverInterface {
     }
 
     public function execute(\Magento\Framework\Event\Observer $observer) {
-
+        $extraFieldObject = $observer->getEvent()->getData('object');
+        $data = $observer->getEvent()->getData('data');
+        if (isset($data['attribute_values']['type_id'])) {
+            if ($data['attribute_values']['type_id'] == 'configurable') {
+                $product = null;
+                $registry = $this->simiObjectManager->get('\Magento\Framework\Registry');
+                if ($registry->registry('product')) {
+                    $product = $registry->registry('current_product');
+                }
+                if ($product && $product->getId()) {
+                    $options = $this->simiObjectManager->get('Simi\Simicustomize\Helper\Options\Configurable')->getOptions($product);
+                    if ($extraFieldObject && $extraFieldObject->extraFields && isset($extraFieldObject->extraFields['app_options'])) {
+                        $extraFieldObject->extraFields['app_options'] = $options;
+                    }
+                }
+            }
+        }
     }
 }
