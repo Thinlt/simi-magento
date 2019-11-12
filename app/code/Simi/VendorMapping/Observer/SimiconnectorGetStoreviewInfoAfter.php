a34d3987dce8459e2648b1cf53f9895a5758bcf4
Robert <robert@Roberts-MacBook-Pro.local>
2019-11-12 11:35:34 +0700

    Fixbug compile error vendorMapping

--- app/code/Simi/VendorMapping/Observer/SimiconnectorGetStoreviewInfoAfter.php
+++ app/code/Simi/VendorMapping/Observer/SimiconnectorGetStoreviewInfoAfter.php
@@ -5,12 +5,6 @@ namespace Simi\VendorMapping\Observer;
 use Magento\Framework\Event\ObserverInterface;
 
 class SimiconnectorGetStoreviewInfoAfter implements ObserverInterface {
-
-    // public function execute(\Magento\Framework\Event\Observer $observer) {
-    //     // add giftcard product timezone options
-    //     $object = $observer->getObject();
-    //     // storeviewInfo
-    //     $object->storeviewInfo['vendor_list'] = ['vendor 1', 'vendor 2'];
-    //     return $this;
-    // }
+    public function execute(\Magento\Framework\Event\Observer $observer) {
+    }
 }
