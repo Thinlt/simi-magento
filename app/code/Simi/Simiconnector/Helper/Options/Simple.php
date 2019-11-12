025020ba894fd781800e6642818a2fb8a12a6d6f
Cody Nguyen <cody@simicart.com>
2019-11-12 15:47:30 +0700

    Check before foreach

--- app/code/Simi/Simiconnector/Helper/Options/Simple.php
+++ app/code/Simi/Simiconnector/Helper/Options/Simple.php
@@ -32,22 +32,42 @@ class Simple extends \Simi\Simiconnector\Helper\Options
         $block_product = $layout->createBlock('Magento\Swatches\Block\Product\Renderer\Configurable\Interceptor');
         $options       = $block_product->decorateArray($product->getOptions());
 
-        foreach ($options as $option) {
-            $item               = [];
-            $item['id']         = $option->getId();
-            $item['title']      = $option->getTitle();
-            $item['type']       = $option->getType();
-            $item['position']   = $option->getSortOrder();
-            $item['isRequired'] = $option->getIsRequire();
-            $this->getExtraInfo($option, $item);
-            if ($option->getGroupByType() == \Magento\Catalog\Model\Product\Option::OPTION_GROUP_SELECT) {
-                foreach ($option->getValues() as $value) {
-                    $item_value = [
-                        'id'    => $value->getId(),
-                        'title' => $value->getTitle(),
-                    ];
-                    $price      = $value->getPrice(true);
+        if ($options && is_array($options)) {
+            foreach ($options as $option) {
+                $item               = [];
+                $item['id']         = $option->getId();
+                $item['title']      = $option->getTitle();
+                $item['type']       = $option->getType();
+                $item['position']   = $option->getSortOrder();
+                $item['isRequired'] = $option->getIsRequire();
+                $this->getExtraInfo($option, $item);
+                if ($option->getGroupByType() == \Magento\Catalog\Model\Product\Option::OPTION_GROUP_SELECT) {
+                    foreach ($option->getValues() as $value) {
+                        $item_value = [
+                            'id'    => $value->getId(),
+                            'title' => $value->getTitle(),
+                        ];
+                        $price      = $value->getPrice(true);
 
+                        $_priceInclTax = $this->currency($this->getPrice($product, $price, true), false, false);
+                        $_priceExclTax = $this->currency($this->getPrice($product, $price), false, false);
+
+                        if ($taxHelper->displayPriceIncludingTax()) {
+                            $this->helper('Simi\Simiconnector\Helper\Price')->setTaxPrice($item_value, $_priceInclTax);
+                        } elseif ($taxHelper->displayPriceExcludingTax()) {
+                            $this->helper('Simi\Simiconnector\Helper\Price')->setTaxPrice($item_value, $_priceExclTax);
+                        } elseif ($taxHelper->displayBothPrices()) {
+                            $this->helper('Simi\Simiconnector\Helper\Price')
+                                    ->setBothTaxPrice($item_value, $_priceExclTax, $_priceInclTax);
+                        } else {
+                            $this->helper('Simi\Simiconnector\Helper\Price')->setTaxPrice($item_value, $_priceInclTax);
+                        }
+
+                        $item['values'][] = $item_value;
+                    }
+                } else {
+                    $item_value    = [];
+                    $price         = $option->getPrice(true);
                     $_priceInclTax = $this->currency($this->getPrice($product, $price, true), false, false);
                     $_priceExclTax = $this->currency($this->getPrice($product, $price), false, false);
 
@@ -61,29 +81,11 @@ class Simple extends \Simi\Simiconnector\Helper\Options
                     } else {
                         $this->helper('Simi\Simiconnector\Helper\Price')->setTaxPrice($item_value, $_priceInclTax);
                     }
-
                     $item['values'][] = $item_value;
                 }
-            } else {
-                $item_value    = [];
-                $price         = $option->getPrice(true);
-                $_priceInclTax = $this->currency($this->getPrice($product, $price, true), false, false);
-                $_priceExclTax = $this->currency($this->getPrice($product, $price), false, false);
 
-                if ($taxHelper->displayPriceIncludingTax()) {
-                    $this->helper('Simi\Simiconnector\Helper\Price')->setTaxPrice($item_value, $_priceInclTax);
-                } elseif ($taxHelper->displayPriceExcludingTax()) {
-                    $this->helper('Simi\Simiconnector\Helper\Price')->setTaxPrice($item_value, $_priceExclTax);
-                } elseif ($taxHelper->displayBothPrices()) {
-                    $this->helper('Simi\Simiconnector\Helper\Price')
-                            ->setBothTaxPrice($item_value, $_priceExclTax, $_priceInclTax);
-                } else {
-                    $this->helper('Simi\Simiconnector\Helper\Price')->setTaxPrice($item_value, $_priceInclTax);
-                }
-                $item['values'][] = $item_value;
+                $info[] = $item;
             }
-
-            $info[] = $item;
         }
         $options                   = [];
         $options['custom_options'] = $info;
