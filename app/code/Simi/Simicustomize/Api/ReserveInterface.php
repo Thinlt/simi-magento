bbad14765f148fc4cda110c2376b0dd1caa1be5e
Robert <robert@Roberts-MacBook-Pro.local>
2019-11-14 00:48:37 +0700

    Reserve backend function with send reservation email to customer

---
+++ app/code/Simi/Simicustomize/Api/ReserveInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Simi\Simicustomize\Api;
+
+interface ReserveInterface
+{
+    /**
+     * Save Reserve request
+     * @return boolean
+     */
+    public function index();
+}
---
+++ app/code/Simi/Simicustomize/Model/Api/Reserve.php
@@ -0,0 +1,190 @@
+<?php
+namespace Simi\Simicustomize\Model\Api;
+
+class Reserve extends \Simi\Simiconnector\Model\Api\Apiabstract implements \Simi\Simicustomize\Api\ReserveInterface
+{
+    /**
+     * \Magento\Framework\App\RequestInterface
+     */
+    protected $request;
+
+    /**
+    * @var \Magento\Framework\App\Config\ScopeConfigInterface
+    */
+    protected $config;
+
+    /**
+    * @var \Magento\Framework\Stdlib\DateTime\DateTime
+    */
+    protected $date;
+
+    /**
+    * @var \Magento\Framework\Mail\Template\TransportBuilder
+    */
+    protected $transportBuilder;
+
+    /**
+    * @var \Magento\Framework\Translate\Inline\StateInterface
+    */
+    protected $inlineTranslation;
+
+    public function __construct(
+        \Magento\Framework\ObjectManagerInterface $simiObjectManager,
+        \Magento\Framework\App\Config\ScopeConfigInterface $config,
+        \Magento\Framework\Stdlib\DateTime\DateTime $date,
+        \Magento\Framework\Mail\Template\TransportBuilder $transportBuilder,
+        \Magento\Framework\Translate\Inline\StateInterface $inlineTranslation,
+        \Magento\Framework\App\RequestInterface $request
+    ){
+        $this->request = $request;
+        $this->config = $config;
+        $this->date = $date;
+        $this->transportBuilder = $transportBuilder;
+        $this->inlineTranslation = $inlineTranslation;
+        parent::__construct($simiObjectManager);
+    }
+
+    /**
+     * Set builder query
+     * @return boolean
+     */
+    public function setBuilderQuery()
+    {
+        $this->builderQuery = false;
+    }
+
+    /**
+     * Save Reserve request
+     * @return boolean
+     */
+    public function index() {
+        $reserve = $this->simiObjectManager->get('\Simi\Simicustomize\Model\Reserve');
+        $data = $this->request->getParams();
+        if ($this->request->getContent()) {
+            $data = json_decode($this->request->getContent(), true);
+        }
+        if (isset($data['product_id']) && isset($data['storelocator_id']) && isset($data['customer_id'])) {
+            $reserve->setData('product_id', $data['product_id']);
+            $reserve->setData('product_name', $data['product_name']);
+            $reserve->setData('storelocator_id', $data['storelocator_id']);
+            $reserve->setData('store_name', $data['store_name']);
+            $reserve->setData('customer_id', $data['customer_id']);
+            $reserve->setData('customer_name', $data['customer_name']);
+            $collection = $reserve->getCollection();
+            $collection->addFieldToFilter('product_id', $data['product_id'])
+                ->addFieldToFilter('storelocator_id', $data['storelocator_id'])
+                ->addFieldToFilter('customer_id', $data['customer_id'])
+                ->load();
+            if ($collection->getSize()) {
+                $item = $collection->getFirstItem();
+                // compare with date format 'Y-m-d H:i:s'
+                if ($this->date->gmtDate('Y-m-d', $item->getData('date')) == $this->date->gmtDate('Y-m-d')) {
+                    $reserve = $item;
+                }
+            }
+            if (isset($data['request_info'])) {
+                if (isset($data['request_info']['product']) && isset($data['request_info']['super_attribute'])) {
+                    $_product = $this->simiObjectManager->create('\Magento\Catalog\Model\Product')->load($data['request_info']['product']);
+                    $optionInfos = [];
+                    $attributes = $data['request_info']['super_attribute'];
+                    foreach($attributes as $code => $optionId){
+                        $attr = $_product->getResource()->getAttribute($code);
+                        if ($attr->usesSource()) {
+                            $optionText = $attr->getSource()->getOptionText($optionId);
+                            $optionInfos[] = $attr->getAttributeCode().' '.$optionText;
+                        }
+                    }
+                    $reserve->setData('request_info', implode(', ', $optionInfos));
+                }
+            }
+            // calculate next date for reservation date
+            $reserve->setData('date', $this->date->gmtDate());
+            $reserve->setData('reservation_date', $this->_getNextWorkingDay());
+            try{
+                $reserve->save();
+                
+                // send email to customer
+                $customer = $this->simiObjectManager->create('\Magento\Customer\Model\Customer')->load($data['customer_id']);
+                if ($customer->getEmail()) {
+                    $this->inlineTranslation->suspend();
+                    $postObject = new \Magento\Framework\DataObject();
+                    $postObject->setData($reserve->getData());
+                    $postObject->setData('customer_email', $customer->getEmail());
+                    $postObject->setData('email', $customer->getEmail());
+                    
+                    $error = false;
+                    $storeScope = \Magento\Store\Model\ScopeInterface::SCOPE_STORE;
+                    
+                    $sender = [
+                        'name' => $this->config->getValue('trans_email/ident_sales/name', $storeScope),
+                        'email' => $this->config->getValue('trans_email/ident_sales/email', $storeScope),
+                    ];
+                    
+                    $transport = $this->transportBuilder
+                        ->setTemplateIdentifier($this->config->getValue('sales/reserve/emailTemplate', $storeScope)) // this code we have mentioned in the email_templates.xml
+                        ->setTemplateOptions([
+                            'area' => \Magento\Framework\App\Area::AREA_ADMINHTML, // this is using frontend area to get the template file
+                            'store' => \Magento\Store\Model\Store::DEFAULT_STORE_ID,
+                        ])
+                        ->setTemplateVars(['data' => $postObject])
+                        ->setFrom($sender)
+                        ->addTo($customer->getEmail())
+                        ->getTransport();
+                    
+                    $transport->sendMessage();
+                    $this->inlineTranslation->resume();
+                }
+                
+                return true;
+            }catch(\Exception $e){
+                return false;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * calculate next date for reservation date
+     */
+    protected function _getNextWorkingDay(){
+        $submitDate = new \DateTime();
+        $submitDate->modify('+1 day');
+        $working_days = $this->config->getValue('sales/reserve/working_days');
+        if ($working_days) {
+            $working_days = str_replace(' ', '', strtolower($working_days));
+            $working_days = explode(',', $working_days);
+        } else {
+            $working_days = [];
+        }
+        $holidays = $this->config->getValue('sales/reserve/holiday');
+        if ($holidays) {
+            $holidays = str_replace(' ', '', strtolower($holidays));
+            $holidays = explode(',', $holidays);
+        } else {
+            $holidays = [];
+        }
+        $date = $this->date->gmtDate(null, $submitDate->format('Y-m-d H:i:s'));
+        // next date for workday
+        $workday = date('D', $this->date->gmtTimestamp($date));
+        if (count($working_days)){ 
+            for($i = 0; $i<8; $i++){
+                if (!in_array(strtolower($workday), $working_days)) {
+                    $submitDate->modify('+1 day');
+                    $date = $this->date->gmtDate(null, $submitDate->format('Y-m-d H:i:s'));
+                    $workday = date('D', $this->date->gmtTimestamp($date));
+                }
+            }
+        }
+        // next date for holiday
+        if (count($holidays)){ 
+            for($i = 0; $i<count($holidays); $i++){
+                $checkDateHoliday = $submitDate->format('j/n'); //date/month without leading zeros
+                if (in_array($checkDateHoliday, $holidays)) {
+                    $submitDate->modify('+1 day');
+                    $date = $this->date->gmtDate(null, $submitDate->format('Y-m-d H:i:s'));
+                }
+            }
+        }
+        return $date;
+    }
+}
