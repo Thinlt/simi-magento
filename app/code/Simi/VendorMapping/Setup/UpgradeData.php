99ec94db932955aa73676913479124070b6c0dff
namhoang2201 <hoangnamtb95@gmail.com>
2020-01-02 15:56:56 +0700

    Revert Vnecoms/Vendors module.

--- app/code/Simi/VendorMapping/Setup/UpgradeData.php
+++ app/code/Simi/VendorMapping/Setup/UpgradeData.php
@@ -1,19 +1,23 @@
 <?php
 
+/**
+ * Copyright © 2020 Simicart. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
 namespace Simi\VendorMapping\Setup;
 
+use Exception;
 use Magento\Framework\Setup\UpgradeDataInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Eav\Setup\EavSetup;
-use Magento\Catalog\Model\Product\Attribute\Source\Status;
 
-// use Magento\Catalog\Model\Product;
-// use Aheadworks\Giftcard\Api\Data\ProductAttributeInterface;
-// use Magento\Framework\DB\Ddl\Table;
-// use Magento\Catalog\Model\ResourceModel\Eav\Attribute as CatalogEavAttribute;
-// use Magento\Framework\DB\Adapter\AdapterInterface;
 
+/**
+ * Upgrade Data script
+ * @codeCoverageIgnore
+ */
 class UpgradeData implements UpgradeDataInterface
 {
     /**
@@ -22,17 +26,15 @@ class UpgradeData implements UpgradeDataInterface
     private $eavSetup;
 
     /**
-       * @param EavSetup $eavSetup
-       * @param QuoteSetupFactory $setupFactory
-       * @param SalesSetupFactory $salesSetupFactory
-       */
-      public function __construct(
+     * @param EavSetup $eavSetup
+     * @param QuoteSetupFactory $setupFactory
+     * @param SalesSetupFactory $salesSetupFactory
+     */
+    public function __construct(
         EavSetup $eavSetup
     ) {
         $this->eavSetup = $eavSetup;
     }
-
-    
 
     /**
      * Upgrades data for a module
@@ -41,42 +43,46 @@ class UpgradeData implements UpgradeDataInterface
      * @param ModuleContextInterface $context
      * @return void
      */
-    public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context){
-        if ($context->getVersion() && version_compare($context->getVersion(), '0.1.1', '<')) {
-            $attributes = [
-                'is_admin_sell' => [
-                    // 'group'              => '',
-                    'input'              => 'boolean',
-                    'type'               => 'int',
-                    'label'              => 'Sell by Admin',
-                    'visible'            => true,
-                    'required'           => false,
-                    'user_defined'               => false,
-                    'searchable'                 => false,
-                    'filterable'                 => true,
-                    'comparable'                 => false,
-                    'visible_on_front'           => false,
-                    'visible_in_advanced_search' => false,
-                    'is_html_allowed_on_front'   => false,
-                    'used_for_promo_rules'       => true,
-                    // 'source'                     => Status::class,
-                    'source' => \Magento\Eav\Model\Entity\Attribute\Source\Boolean::class,
-                    'default' => \Magento\Eav\Model\Entity\Attribute\Source\Boolean::VALUE_YES,
-                    // 'frontend_class'             => '',
-                    'global'                     =>  \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_GLOBAL,
-                    'unique'                     => false,
-                    'apply_to'                   => 'simple,grouped,configurable,downloadable,virtual,bundle,aw_giftcard'
-                ]
-            ];
-    
-            // $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
-            foreach ($attributes as $attribute_code => $attributeOptions) {
-                $this->eavSetup->addAttribute(
-                    \Magento\Catalog\Model\Product::ENTITY,
-                    $attribute_code,
-                    $attributeOptions
-                );
+    public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        try {
+            if ($context->getVersion() && version_compare($context->getVersion(), '0.1.3', '<')) {
+
+                $attributes = [
+                    'website' => [
+                        'type' => \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                        'nullable' => true,
+                        'length' =>  255,
+                        'comment' => 'Website Url',
+                        'label' => 'Website'
+                    ],
+                    'facebook' => [
+                        'type' => \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                        'nullable' => true,
+                        'length' =>  255,
+                        'comment' => 'Facebook Url',
+                        'label' => 'Facebook'
+                    ],
+                    'instagram' => [
+                        'type' => \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                        'nullable' => true,
+                        'length' =>  255,
+                        'comment' => 'Instagram Url',
+                        'label' => 'Instagram'
+                    ]
+                ];
+
+                // $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+                foreach ($attributes as $attribute_code => $attributeOptions) {
+                    $this->eavSetup->addAttribute(
+                        \Vnecoms\Vendors\Model\Vendor::ENTITY,
+                        $attribute_code,
+                        $attributeOptions
+                    );
+                }
             }
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage());
         }
     }
 }
--- app/code/Vnecoms/Vendors/Setup/UpgradeData.php
+++ app/code/Vnecoms/Vendors/Setup/UpgradeData.php
@@ -1,18 +1,19 @@
 <?php
-
 /**
  * Copyright © 2016 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-
 namespace Vnecoms\Vendors\Setup;
-
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
 use Magento\Framework\Setup\UpgradeDataInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Eav\Setup\EavSetup;
-
-
+use Magento\Eav\Setup\EavSetupFactory;
+use Vnecoms\Vendors\Model\Vendor;
+use Vnecoms\Vendors\Setup\VendorSetupFactory;
+use Magento\Eav\Model\Entity\Attribute\Set as AttributeSet;
+use Magento\Eav\Model\Entity\Attribute\SetFactory as AttributeSetFactory;
 /**
  * Upgrade Data script
  * @codeCoverageIgnore
@@ -20,64 +21,68 @@ use Magento\Eav\Setup\EavSetup;
 class UpgradeData implements UpgradeDataInterface
 {
     /**
-     * @var EavSetup
+     * Vendor setup factory
+     *
+     * @var VendorSetupFactory
      */
-    private $eavSetup;
-
+    private $vendorSetupFactory;
     /**
-     * @param EavSetup $eavSetup
-     * @param QuoteSetupFactory $setupFactory
-     * @param SalesSetupFactory $salesSetupFactory
+     * EAV setup factory
+     *
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+    private $attributeSetFactory;
+    /**
+     * Init
+     *
+     * @param CategorySetupFactory $categorySetupFactory
+     * @param EavSetupFactory $eavSetupFactory
      */
     public function __construct(
-        EavSetup $eavSetup
+        VendorSetupFactory $vendorSetupFactory,
+        EavSetupFactory $eavSetupFactory,
+        AttributeSetFactory $attributeSetFactory
     ) {
-        $this->eavSetup = $eavSetup;
+    
+        $this->vendorSetupFactory = $vendorSetupFactory;
+        $this->eavSetupFactory = $eavSetupFactory;
+        $this->attributeSetFactory = $attributeSetFactory;
     }
-
     /**
-     * Upgrades data for a module
-     *
-     * @param ModuleDataSetupInterface $setup
-     * @param ModuleContextInterface $context
-     * @return void
+     * {@inheritdoc}
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
      */
     public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
     {
-        if ($context->getVersion() && version_compare($context->getVersion(), '2.2.2', '<')) {
-
-            $attributes = [
-                'website' => [
-                    'type' => \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-                    'nullable' => true,
-                    'length' =>  255,
-                    'comment' => 'Website Url',
-                    'label' => 'Website'
-                ],
-                'facebook' => [
-                    'type' => \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-                    'nullable' => true,
-                    'length' =>  255,
-                    'comment' => 'Facebook Url',
-                    'label' => 'Facebook'
-                ],
-                'instagram' => [
-                    'type' => \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-                    'nullable' => true,
-                    'length' =>  255,
-                    'comment' => 'Instagram Url',
-                    'label' => 'Instagram'
+        $setup->startSetup();
+        $ticketSetup = $this->vendorSetupFactory->create(['setup' => $setup]);
+        if ($context->getVersion()
+            && version_compare($context->getVersion(), '2.1.2') < 0
+        ) {
+            $ticketSetup->addAttribute(
+                'vendor',
+                'flag_notify_email',
+                [
+                    'label' => 'Flag Notify Email',
+                    'type' => 'varchar',
+                    'input' => 'text',
+                    'required' => true,
+                    'sort_order' => 70,
+                    'position' => 40,
+                    'default'=>0
                 ]
-            ];
-
-            // $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
-            foreach ($attributes as $attribute_code => $attributeOptions) {
-                $this->eavSetup->addAttribute(
-                    \Vnecoms\Vendors\Model\Vendor::ENTITY,
-                    $attribute_code,
-                    $attributeOptions
-                );
-            }
+            );
         }
+        $setup->endSetup();
+    }
+    /**
+     * Create page
+     *
+     * @return Page
+     */
+    public function createSpam()
+    {
+        return $this->spamFactory->create();
     }
 }
